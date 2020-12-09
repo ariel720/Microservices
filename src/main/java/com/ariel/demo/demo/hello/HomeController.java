b3b9197891842a2a1784ade0adbfea178231fcf5
Shinhee Kim <Shinhee.Kim@GeorgianCollege.ca>
2020-12-09 16:53:31 -0500

    message testing

--- src/main/java/com/ariel/demo/demo/hello/HomeController.java
+++ src/main/java/com/ariel/demo/demo/hello/HomeController.java
@@ -1,13 +1,20 @@
 package com.ariel.demo.demo.hello;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+	private MessageSource messageSource;
+	
     @RequestMapping("/")
     public String home(){
         return "Hello Home!";
@@ -31,5 +38,10 @@ public class HomeController {
         return new HelloWorldBean(String.format("Your name is %s", name));
     }
     
+    @GetMapping("/international")
+    public String international(@RequestHeader(name="Accpet-Language", required=false) Locale locale){
+    	return "hello";
+    	//return messageSource.getMessage("morning.message", null, LocaleContextHolder.getLocale());
+    }
     
 }
